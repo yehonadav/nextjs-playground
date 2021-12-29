07aabf76ece68853f62e3272445ed2389dfa4689
yonadav <yonadav.barilan@gmail.com>
2021-12-30 01:35:45 +0200

    poc for nextjs+zustand

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {FC} from "react";
 import createStore from "zustand";
+import {useRouter} from "next/router";
 
 type State = {
   count: number;
@@ -12,6 +13,7 @@ const useStore = createStore<State>(() => ({
 
 export const Header:FC = () => {
   const count = useStore(s => s.count)
+  const router = useRouter();
   return (
     <h1 className="title">
       <span onClick={() => useStore.setState(s=>({count: s.count+1}))}>
@@ -20,7 +22,10 @@ export const Header:FC = () => {
       <Link href="/posts/first-post">
         <a>this page!</a>
       </Link>
-      {' '}{count}
+      <span onClick={(e) => {
+        e.preventDefault();
+        router.push("/posts/first-post");
+      }}>{' '}{count}</span>
     </h1>
   )
 }
