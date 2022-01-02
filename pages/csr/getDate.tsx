79d36eac2287ba2494a114825f196262fc3cb0a0
yonadav <yonadav.barilan@gmail.com>
2022-01-02 17:21:22 +0200

    tests with ssg props

---
+++ pages/csr/getDate.tsx
@@ -0,0 +1,11 @@
+import useSWR from 'swr'
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Profile() {
+  const { data, error, mutate, isValidating } = useSWR('/api/getDate', fetcher)
+  console.log({ data, error, mutate, isValidating });
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return <h3>{data.date}</h3>
+}
