419df7186e29c619d61f454beae0225df10f2dff
yonadav <yonadav.barilan@gmail.com>
2022-01-02 15:10:53 +0200

    tests with ssg props

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,20 @@ import {Header} from "../components/Header";
 import Image from 'next/image'
 import Layout, {siteTitle} from "../components/layouts/Layout";
 import utilStyles from '../styles/utils.module.css'
+import {getSortedPostsData} from "../lib/posts";
+import {DateFormat} from "../components/DateFormat";
+import Link from 'next/link'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
     <div className="container">
@@ -26,6 +38,23 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <DateFormat dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
 
         <Image
           src="/images/profile.jpg" // Route of the image file
