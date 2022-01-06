97d793ff67d72308152ec992a6193ea685f5cf78
yonadav <yonadav.barilan@gmail.com>
2022-01-06 08:08:15 +0200

    adding report vitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
 import '../styles/global.css'
-import type { AppProps /*, AppContext */ } from 'next/app'
+import type {AppProps, NextWebVitalsMetric /*, AppContext */} from 'next/app'
 // import variables from '../styles/scss/variables.module.scss'
+
+export function reportWebVitals(metric:NextWebVitalsMetric ) {
+  console.log("WebVital Metric:", metric);
+
+  // const body = JSON.stringify(metric)
+  // const url = 'https://example.com/analytics'
+  //
+  // // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  // if (navigator.sendBeacon) {
+  //   navigator.sendBeacon(url, body)
+  // } else {
+  //   fetch(url, { body, method: 'POST', keepalive: true })
+  // }
+
+  // const { id, name, label, value } = metric;
+  // // @ts-ignore
+  // // Use `window.gtag` if you initialized Google Analytics as this example:
+  // // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
+  // window.gtag('event', name, {
+  //   event_category:
+  //     label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+  //   value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+  //   event_label: id, // id unique to current page load
+  //   non_interaction: true, // avoids affecting bounce rate.
+  // })
+}
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
