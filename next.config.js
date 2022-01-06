b27a7c02cd6cdcfbd3ee0f78c04f32c1d97a6a57
yonadav <yonadav.barilan@gmail.com>
2022-01-06 19:17:44 +0200

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -8,6 +8,42 @@
 // @ts-check
 const path = require('path')
 
+// You can choose which headers to add to the list
+// after learning more below.
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+];
+
+// When enabled two HTML files (client.html and server.html)
+// will be outputted to <distDir>/analyze/. One will be for the server
+// bundle, one for the browser bundle.
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
@@ -24,11 +60,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'assets/styles/scss')],
+    includePaths: [path.join(__dirname, 'src/styles/scss')],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
-const sentryWebpackPluginOptions = {
+// const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
   // recommended:
@@ -36,14 +81,14 @@ const sentryWebpackPluginOptions = {
   //   urlPrefix, include, ignore
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+// }
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withMDX({
+module.exports = withBundleAnalyzer(withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   // withSentryConfig(
     ...nextConfig
     // , sentryWebpackPluginOptions
   // )
-})
+}))
