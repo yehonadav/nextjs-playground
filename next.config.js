60359c8ecb0be589cb0ddd9e95696602b3275c2a
yonadav <yonadav.barilan@gmail.com>
2022-01-27 21:11:24 +0200

    try pwa from scratch

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const withPWA = require('next-pwa')
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withPWA({
-  pwa: {
-    // pwa output folder
-    // dest: '.next/pwa'
-    // 
-    // Other configurations:
-    // ...
-  }
-})
+module.exports = nextConfig
