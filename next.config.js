ce8e906f611031fb56d99749d55cb57eb6453cc4
yonadav <yonadav.barilan@gmail.com>
2022-01-29 10:03:43 +0200

    added notifications

--- next.config.js
+++ next.config.js
@@ -72,6 +72,13 @@ nextConfig = require('next-pwa')({...nextConfig,
     dest: "public",
     register: true,
     skipWaiting: true,
+    fallbacks: {
+      image: '/images/fallback.png',
+      // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
+      // font: '/static/font/fallback.woff2',
+      // audio: ...,
+      // video: ...,
+    },
   },
 });
 
